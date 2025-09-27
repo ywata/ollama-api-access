8650b7356af65e2d4d6712b44234fc5204fa816a
Yasu Watanabe <ywata@protonmail.com>
2025-09-27 17:41:00 +0900

    feat: add Nix flake configuration for Rust project with Ollama API access

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Rust project for Ollama API access";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rustfmt" "clippy" ];
+        };
+
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          openssl
+        ] ++ lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+          
+          shellHook = ''
+            echo "🦀 Rust development environment loaded!"
+            echo "Rust version: $(rustc --version)"
+            echo "Cargo version: $(cargo --version)"
+          '';
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "ollama-api-access";
+          version = "0.1.0";
+          
+          src = ./.;
+          
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          
+          inherit nativeBuildInputs buildInputs;
+          
+          meta = with pkgs.lib; {
+            description = "Rust application for Ollama API access";
+            license = licenses.mit;
+            maintainers = [ ];
+          };
+        };
+      });
+}
