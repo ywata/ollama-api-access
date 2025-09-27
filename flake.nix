3f8272b8e6dfeb32a08c0e680994e0abaa1abf17
Yasu Watanabe <ywata@protonmail.com>
2025-09-27 19:09:46 +0900

    chore: initialize Rust project with Ollama API dependencies and Nix config

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Rust project for Ollama API access";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -22,13 +22,15 @@
         nativeBuildInputs = with pkgs; [
           rustToolchain
           pkg-config
+          ollama
         ];
 
         buildInputs = with pkgs; [
           openssl
-        ] ++ lib.optionals stdenv.isDarwin [
-          darwin.apple_sdk.frameworks.Security
-          darwin.apple_sdk.frameworks.SystemConfiguration
+        ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+          pkgs.darwin.apple_sdk.frameworks.Security
+          pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
         ];
 
       in
@@ -40,26 +42,28 @@
             echo "🦀 Rust development environment loaded!"
             echo "Rust version: $(rustc --version)"
             echo "Cargo version: $(cargo --version)"
+            echo "Available tools: rustfmt, clippy, rust-analyzer"
           '';
         };
 
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "ollama-api-access";
-          version = "0.1.0";
-          
-          src = ./.;
-          
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-          
-          inherit nativeBuildInputs buildInputs;
-          
-          meta = with pkgs.lib; {
-            description = "Rust application for Ollama API access";
-            license = licenses.mit;
-            maintainers = [ ];
-          };
-        };
+        # Uncomment when ready to build the package
+        # packages.default = pkgs.rustPlatform.buildRustPackage {
+        #   pname = "ollama-api-access";
+        #   version = "0.1.0";
+        #   
+        #   src = ./.;
+        #   
+        #   cargoLock = {
+        #     lockFile = ./Cargo.lock;
+        #   };
+        #   
+        #   inherit nativeBuildInputs buildInputs;
+        #   
+        #   meta = with pkgs.lib; {
+        #     description = "Rust application for Ollama API access";
+        #     license = licenses.mit;
+        #     maintainers = [ ];
+        #   };
+        # };
       });
 }
